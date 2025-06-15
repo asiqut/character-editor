4208d9ae0a2df1b102933aebae94e02ee652cb44
asiqut <sofyazemlanaya@gmail.com>
2025-06-15 20:05:37 +0400

    # Экспорт персонажа

---
+++ src/lib/exporter.js
@@ -0,0 +1,11 @@
+export function exportPNG(psdData, character) {
+  console.log('Exporting PNG with:', character);
+  // Реальная реализация будет использовать html-to-image
+  alert('PNG export functionality will be implemented here');
+}
+
+export function exportPSD(psdData, character) {
+  console.log('Exporting PSD with:', character);
+  // Реальная реализация будет использовать psd-write
+  alert('PSD export functionality will be implemented here');
+}
