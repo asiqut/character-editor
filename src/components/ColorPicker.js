c73edc0422e0027b5f5c78a0d7c7a0726df9fb73
asiqut <sofyazemlanaya@gmail.com>
2025-06-15 18:59:36 +0400

    # Компонент выбора цвета

---
+++ src/components/ColorPicker.js
@@ -0,0 +1,30 @@
+// src/components/ColorPicker.js
+import React from 'react';
+import { ChromePicker } from 'react-color';
+
+function ColorPicker({ part, color, onChange }) {
+  const [showPicker, setShowPicker] = React.useState(false);
+  
+  return (
+    <div className="color-picker">
+      <h3>Color for {part}</h3>
+      <div 
+        className="color-swatch" 
+        style={{ backgroundColor: color }}
+        onClick={() => setShowPicker(!showPicker)}
+      />
+      
+      {showPicker && (
+        <ChromePicker
+          color={color}
+          onChangeComplete={(color) => {
+            onChange(part, color.hex);
+            setShowPicker(false);
+          }}
+        />
+      )}
+    </div>
+  );
+}
+
+export default ColorPicker;
