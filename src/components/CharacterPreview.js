d08be57fcd8eaa63b2a8511d2bc898cd61ba3e77
asiqut <sofyazemlanaya@gmail.com>
2025-06-16 02:52:21 +0400

    CharacterPreview.js

--- src/components/CharacterPreview.js
+++ src/components/CharacterPreview.js
@@ -5,12 +5,33 @@ function CharacterPreview({ psdData, character }) {
   const canvasRef = useRef(null);
   
   useEffect(() => {
+    console.log('CharacterPreview useEffect triggered', { psdData, character });
     if (psdData && canvasRef.current) {
+      console.log('Calling renderCharacter');
       renderCharacter(canvasRef.current, psdData, character);
     }
   }, [psdData, character]);
   
-  return <canvas ref={canvasRef} width={800} height={800} />;
+  return (
+    <div style={{ border: '1px solid red', position: 'relative' }}>
+      <canvas 
+        ref={canvasRef} 
+        width={800} 
+        height={800} 
+        style={{ maxWidth: '100%', border: '1px solid blue' }}
+      />
+      <div style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        color: 'red',
+        background: 'white',
+        padding: '5px'
+      }}>
+        Debug Canvas
+      </div>
+    </div>
+  );
 }
 
 export default CharacterPreview;
