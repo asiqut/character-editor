8357b90dc4be9f71dc8a608d18bd7c605c47609f
asiqut <sofyazemlanaya@gmail.com>
2025-06-15 23:04:24 +0400

    psdLoader.js

--- src/lib/psdLoader.js
+++ src/lib/psdLoader.js
@@ -3,6 +3,8 @@ import * as PSD from 'ag-psd';
 export async function loadPSD() {
   try {
     const response = await fetch(`${process.env.PUBLIC_URL}/assets/model_kinwoods.psd`);
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    
     const arrayBuffer = await response.arrayBuffer();
     return PSD.readPsd(arrayBuffer);
   } catch (error) {
