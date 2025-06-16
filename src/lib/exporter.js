ad242e89d5092f20433ccd89c3de2a0b6d2dc530
asiqut <sofyazemlanaya@gmail.com>
2025-06-17 02:04:34 +0400

    exporter.js

--- src/lib/exporter.js
+++ src/lib/exporter.js
@@ -16,8 +16,8 @@ export const exportPNG = (canvas, character, psdData) => {
 
 export const exportPSD = (psdData, character) => {
   const newPsd = {
-    width: 800,
-    height: 800,
+    width: psdData.width || 800,
+    height: psdData.height || 800,
     children: []
   };
 
@@ -38,6 +38,8 @@ export const exportPSD = (psdData, character) => {
         newPsd.children.push({
           name: `${part}/${variant}/${layer.name}`,
           canvas: layer.canvas,
+          left: layer.left, // Сохраняем оригинальные координаты
+          top: layer.top,
           opacity: layer.opacity
         });
       }
