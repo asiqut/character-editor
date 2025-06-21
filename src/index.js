6479e23963889d63b36d643c1857b412dac93c3e
asiqut <sofyazemlanaya@gmail.com>
2025-06-21 16:08:04 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/main.css';
+import { CHARACTER_CONFIG } from './lib/characterConfig';
 
+console.log('CHARACTER_CONFIG loaded:', !!CHARACTER_CONFIG); // Должно быть true
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
