69db582d23ab259e6a5743dfbc4170dd20e8db3a
asiqut <sofyazemlanaya@gmail.com>
2025-06-21 04:58:03 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -95,6 +95,24 @@ const handleColorChange = (colorType, color) => {
         <div className="preview-area">
           <CharacterPreview psdData={psdData} character={character} />
         </div>
+
+              {/* Основные цвета */}
+          <div className="part-group">
+            <ColorPicker
+              title="Основной цвет"
+              color={character.colors.main}
+              onChange={(color) => {
+                handleColorChange('main', color);
+                setCharacter(prev => ({
+                  ...prev,
+                  partColors: {
+                    ears: color,
+                    cheeks: color,
+                    mane: color,
+                    body: color,
+                    tail: color,
+                    head: color
+                  }
         
         <div className="controls">
           {/* Уши */}
@@ -213,24 +231,6 @@ const handleColorChange = (colorType, color) => {
               onChange={(color) => handlePartColorChange('tail', color)}
             />
           </div>
-
-          {/* Основные цвета */}
-          <div className="part-group">
-            <ColorPicker
-              title="Основной цвет"
-              color={character.colors.main}
-              onChange={(color) => {
-                handleColorChange('main', color);
-                setCharacter(prev => ({
-                  ...prev,
-                  partColors: {
-                    ears: color,
-                    cheeks: color,
-                    mane: color,
-                    body: color,
-                    tail: color,
-                    head: color
-                  }
                 }));
               }}
             />
