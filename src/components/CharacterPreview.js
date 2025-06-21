464abdc4b396defe92cc9e94458d505ee61e103c
asiqut <sofyazemlanaya@gmail.com>
2025-06-21 20:07:47 +0400

    Update CharacterPreview.js

--- src/components/CharacterPreview.js
+++ src/components/CharacterPreview.js
@@ -11,12 +11,30 @@ function CharacterPreview({ psdData, character }) {
     const ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
-    // Временная отрисовка - позже заменим на полноценный рендеринг
+    // Временная отрисовка выбранного варианта
     ctx.fillStyle = '#f0f0f0';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
     ctx.fillStyle = '#333';
     ctx.font = '16px Arial';
-    ctx.fillText('Preview will be here', 10, 30);
+    ctx.fillText(`Выбран: ${character.ears}`, 20, 30);
+    
+    // Простая визуализация "ушей"
+    if (character.ears === 'торчком обычные') {
+      ctx.fillStyle = '#a58a67';
+      // Левый ear
+      ctx.beginPath();
+      ctx.moveTo(100, 50);
+      ctx.lineTo(80, 80);
+      ctx.lineTo(100, 70);
+      ctx.fill();
+      // Правый ear
+      ctx.beginPath();
+      ctx.moveTo(200, 50);
+      ctx.lineTo(220, 80);
+      ctx.lineTo(200, 70);
+      ctx.fill();
+    }
     
   }, [psdData, character]);
 
@@ -25,10 +43,4 @@ function CharacterPreview({ psdData, character }) {
       <canvas 
         ref={canvasRef}
         width={315}
-        height={315}
-      />
-    </div>
-  );
-}
-
-export default CharacterPreview;
+        height={
