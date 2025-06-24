286583074e3a493358470f0f77a5dc390f2a3710
asiqut <sofyazemlanaya@gmail.com>
2025-06-25 01:11:09 +0400

    Update CharacterPreview.js

--- src/components/CharacterPreview.js
+++ src/components/CharacterPreview.js
@@ -6,28 +6,17 @@ function CharacterPreview({ psdData, character }) {
   
   useEffect(() => {
     if (psdData && canvasRef.current) {
-      // Для превью используем увеличенный размер
-      canvasRef.current.width = 315;
-      canvasRef.current.height = 315;
       renderCharacter(canvasRef.current, psdData, character);
     }
   }, [psdData, character]);
   
   return (
-    <div style={{
-      width: '315px', // Отображаем в оригинальном размере
-      height: '315px',
-      position: 'relative',
-      border: '1px solid #ddd',
-      overflow: 'hidden' // Обрезаем увеличенное изображение
-    }}>
+    <div style={{ width: '315px', height: '315px', border: '1px solid #ddd' }}>
       <canvas 
         ref={canvasRef} 
         width={315}
         height={315}
-        style={{ 
-          display: 'block'
-        }}
+        style={{ display: 'block', width: '100%', height: '100%' }}
       />
     </div>
   );
