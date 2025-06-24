58cdc18ee2e2231278527fda90d08122b605b7e2
asiqut <sofyazemlanaya@gmail.com>
2025-06-25 02:11:06 +0400

    Update PartSelector.js

--- src/components/PartSelector.js
+++ src/components/PartSelector.js
@@ -2,38 +2,38 @@ import React from 'react';
 
 function PartSelector({
   title,
+  part,
   options,
   current,
   onChange,
-  subtypes,
+  showSubtypes = false,
+  subtypes = [],
   currentSubtype,
   onSubtypeChange
 }) {
   return (
-    <div className="part-selector">
+    <div className={`part-selector ${part}`}>
       <h3>{title}</h3>
-      
-      {/* Основные варианты */}
       <div className="options">
         {options.map(option => (
           <button
             key={option}
             className={option === current ? 'active' : ''}
-            onClick={() => onChange(option)}
+            onClick={() => onChange(part, option)}
           >
             {option}
           </button>
         ))}
       </div>
       
-      {/* Подтипы (если есть) */}
-      {subtypes && (
+      {showSubtypes && (
         <div className="subtypes">
+          <h4>Варианты:</h4>
           {subtypes.map(subtype => (
             <button
               key={subtype}
               className={subtype === currentSubtype ? 'active' : ''}
-              onClick={() => onSubtypeChange(subtype)}
+              onClick={() => onSubtypeChange(part, subtype)}
             >
               {subtype}
             </button>
