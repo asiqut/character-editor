05a0dbe92690eebda9dbc26473deb8f93d310f40
asiqut <sofyazemlanaya@gmail.com>
2025-06-24 22:20:23 +0400

    Create layerResolver.js

---
+++ src/lib/layerResolver.js
@@ -0,0 +1,34 @@
+import { PARTS } from './defaultConfig';
+
+export function getLayersForPart(part, variant, subtype = null) {
+  const partConfig = PARTS[part];
+  if (!partConfig) return [];
+
+  let layers = [];
+  
+  if (partConfig.isSingleVariant) {
+    layers = [...partConfig.layers];
+  } else {
+    const variantConfig = partConfig.variants[variant];
+    if (variantConfig) {
+      layers = [...variantConfig.layers];
+      
+      // Добавляем подтипы для глаз
+      if (part === 'eyes' && variant === 'обычные' && subtype) {
+        const subtypeConfig = variantConfig.subtypes?.[subtype];
+        if (subtypeConfig) {
+          layers = layers.concat(subtypeConfig.layers);
+        }
+      }
+    }
+  }
+  
+  return layers;
+}
+
+export function resolveLayerPath(part, variant, layerName) {
+  if (PARTS[part].isSingleVariant) {
+    return `${PARTS[part].title}/${layerName}`;
+  }
+  return `${PARTS[part].title}/${variant}/${layerName}`;
+}
