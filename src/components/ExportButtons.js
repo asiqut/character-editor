1cfa2a834a7a55e81ad9aee832ff634ba30fb6f2
asiqut <sofyazemlanaya@gmail.com>
2025-06-25 04:32:18 +0400

    Update ExportButtons.js

--- src/components/ExportButtons.js
+++ src/components/ExportButtons.js
@@ -2,11 +2,15 @@
 import React from 'react';
 import { exportPNG, exportPSD } from '../lib/exporter';
 
-function ExportButtons({ character, psdData }) {
+function ExportButtons({ character, psdData, flatLayers }) {
   return (
     <div className="export-buttons">
-      <button onClick={() => psdData && exportPNG(character, psdData)}>Export PNG</button>
-      <button onClick={() => psdData && exportPSD(psdData, character)}>Export PSD</button>
+      <button onClick={() => psdData && flatLayers && exportPNG(character, psdData, flatLayers)}>
+        Export PNG
+      </button>
+      <button onClick={() => psdData && flatLayers && exportPSD(psdData, character, flatLayers)}>
+        Export PSD
+      </button>
     </div>
   );
 }
